64d45e9ce868b5c3e2a9a07d5910eba30800ebca
AbbasShDev <60615320+AbbasShDev@users.noreply.github.com>
2020-04-05 19:44:19 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
+
+$(".orgPrice").keypress(function(e) {
+    //if the letter is not digit then display error and don't type anything
+    if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+      //display error message
+      $("#DigitMasg1").text("ارقام فقط (0-9)").show();
+      return false;
+    }
+
+    if (e.which > 48 || e.which > 57) {
+        //display error message
+        $("#DigitMasg1").text("");
+      }
+  });
+
+  $(".DiscAmount").keypress(function(e) {
+    //if the letter is not digit then display error and don't type anything
+    if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+      //display error message
+      $("#DigitMasg2").text("ارقام فقط (0-9)").show();
+      return false;
+    }
+
+    if (e.which > 48 || e.which > 57) {
+        //display error message
+        $("#DigitMasg2").text("");
+      }
+  });
+
 $(".calc").on("click" , function(){
 
     if ($(".orgPrice").val() === "") {
@@ -20,8 +49,8 @@ $(".calc").on("click" , function(){
     }else {
     var discPrice = orgPrice1 * DiscAmount1 / 100;
     var result1 = orgPrice1 - discPrice;
-    $(".displayOriginal").text(orgPrice1 + "$");
-    $(".displayDiscount").text(discPrice + "$");
-    $(".displayResult").text(result1 + "$");
+    $(".displayOriginal").text(orgPrice1);
+    $(".displayDiscount").text(discPrice);
+    $(".displayResult").text(result1);
     }
 });
